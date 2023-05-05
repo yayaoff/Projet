9e8d886a4c143684306db3f21837d6f7225284b1
yayaoff <yasminescimmia@hotmail.it>
2023-05-05 17:13:39 +0200

    fdg

--- Project_1731_Part2.py
+++ Project_1731_Part2.py
@@ -81,87 +81,6 @@ def hist_estimators(filename,N):
     fig.tight_layout()
     plt.savefig('plots/estimators_'+filename+'.png')
 
-
-# def plot_estimators(filename,mu,beta,M):
-#     df = pd.read_csv('data/'+filename)
-#     locs = df.iloc[:, 0]
-#     scales = df.iloc[:, 1]
-#     print(locs)
-#     conv_rates_loc = [np.abs(loc - mu) for loc in locs]
-#     conv_rates_scale = [np.abs(scale - beta) for scale in scales]
-#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,15))
-#     ax1.plot(conv_rates_loc)
-#     ax1.set_xlabel('Sample ize')
-#     ax1.set_ylabel('Relative Convergence Rate of Loc')
-#     ax2.plot(conv_rates_scale)
-#     ax2.set_xlabel('Sample size')
-#     ax2.set_ylabel('Relative Convergence Rate of Scale')
-#     fig.tight_layout()
-#     plt.savefig('plots/conv_rates_'+filename+'.png')
-
-# def variances_converge(file,N_rep):
-#     i=1
-#     mu_arr = []
-#     beta_arr = []
-#     mu_var = []
-#     beta_var = []
-#     while(i<N_rep):
-#         f = 'estimators_'+str(i)+'.csv'
-#         mu_k,beta_k = estimators(f,1,2,i)
-#         mu_arr.append(mu_k)
-#         beta_arr.append(beta_k)
-#         var_mu_k = np.var(mu_arr)
-#         var_beta_k = np.var(beta_arr)
-#         mu_var.append(var_mu_k)
-#         beta_var.append(var_beta_k)
-#         # for filename in os.listdir('data'):
-#         #     df = pd.read_csv('data/'+filename)
-#         #     locs = df.iloc[:, 0]
-#         #     scales = df.iloc[:, 1]
-#         write_var_to_csv(file,var_mu_k,var_beta_k,i)
-#         i+=200
-    
-#     return np.var(mu_arr),np.var(beta_arr)
-
-def hist_err_var(filename,N):
-    df = pd.read_csv('data/'+filename+'.csv')
-    locs_var = np.sort(df.iloc[:, 0])
-    scales_var = np.sort(df.iloc[:, 1])
-    mu_bound = df.iloc[:, 2]
-    beta_bound = df.iloc[:, 3]
-
-    freq_locs_var = {}
-    for i in range(len(locs_var)):
-        err = round(np.abs(locs_var[i] - mu_bound[i]),2)
-        if err not in freq_locs_var.keys():
-            freq_locs_var[err] = 1
-        else :
-            freq_locs_var[err] += 1
-
-    freq_scale_var = {}
-    for i in range(len(scales_var)):
-        err = round(np.abs(scales_var[i] - beta_bound[i]),2)
-        if err not in freq_scale_var.keys():
-            freq_scale_var[err] = 1
-        else :
-            freq_scale_var[err] += 1
-
-    arr_loc_var = list(freq_locs_var.values())
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
-    fig.suptitle('Error values for size N='+str(N),fontsize=(15))
-    ax1.set_xlabel('Mu value')
-    ax1.set_ylabel('Frequency')
-    ax1.hist(np.sort(arr_loc_var),bins=5)
-    
-    ax2.set_xlabel('Beta value')
-    ax2.set_ylabel('Frequency')
-    ax2.bar(freq_scale_var.keys(),freq_scale_var.values())
-    
-    fig.tight_layout()
-
-    plt.savefig('plots/'+filename+'.png')
-
-
 def plot_variances_convergence(filename,N,M):
     df = pd.read_csv('data/'+filename+'.csv')
     var_locs = df.iloc[:, 0]
@@ -201,30 +120,42 @@ def GumbelMean(mu, beta):
 def GumbelVar(mu, beta):
     return (PI**2)/6*beta**2
 
-def plot_normal_gumbel(mu,sigma,loc,beta,N):
-    
-    normal = np.random.normal(mu, sigma, N)
-    pdf_norm = norm.pdf(normal, mu, sigma)
+def plot_normal_gumbel(mu,beta):
+    #values to plot the pdfs
+    x = np.linspace(-10, 10, 1000)
 
-    gumbel = np.random.gumbel(loc=loc,scale=beta)
+    #computing the mean and variance of the gumbel 
     gumbel_mean = GumbelMean(mu,beta)
     gumbel_var = GumbelVar(mu,beta)
-    pdf_gumb = gumbel_r.pdf(gumbel, mu, beta)
 
+    #computing the pdfs
+    pdf_gumb = gumbel_r.pdf(x, loc=mu, scale=beta)
+    pdf_norm=norm.pdf(x, loc=gumbel_mean, scale=np.sqrt(gumbel_var))
+
+    #plots
+    plt.plot(x, pdf_gumb, label='Gumbel')
+    plt.plot(x, pdf_norm, label='Normal')
+    plt.xlabel('x')
+    plt.ylabel('PDF, f(x)')
+    plt.title('Comparing Gumbel and Normal Distributions')
+    plt.legend()
+    plt.savefig('Gumb_Norm_'+str(beta)+'.png')
+    
+plot_normal_gumbel(0,3.5)
 
 # Question c
 
-# N1 = [50,100,200,500,1000]
-# M1 = 500
+N1 = [50,100,200,500,1000]
+M1 = 500
 
-# for size in N1:
-#     file = 'data_'+str(size)
-#     for i in range(M1):
-#         data_i = estimators(file,1,2,size,0)
-#     hist_estimators(file,size)
+for size in N1:
+    file = 'data_'+str(size)
+    for i in range(M1):
+        data_i = estimators(file,1,2,size,0)
+    hist_estimators(file,size)
 
 # Question d
-N2 = [50,100,500,1000,2000,3000]
+N2 = [50,100,500,1000,3000]
 M2 = 10**4
 for size in N2:
     file='variances_'+str(size)
@@ -245,10 +176,6 @@ for size in N2:
 
         bounds = CramerRaoBounds(1,2,size)
         write_var_to_csv('data/'+file+'.csv',var_mu_k,var_beta_k,bounds[0],bounds[1])
-
-    #hist_err_var(file,size)
         
     plot_variances_convergence(file,size,M2)
     
-# data2 = estimators('data2.csv',1,2,10000,1)
-# plot_variances_convergence('variances.csv',data2[2],data2[3])
