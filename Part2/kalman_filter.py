65a6d429f956a2501dfb4635b1163db49271094e
yayaoff <yasminescimmia@hotmail.it>
2023-05-06 16:32:06 +0200

    k

--- Part2/kalman_filter.py
+++ Part2/kalman_filter.py
@@ -14,6 +14,36 @@ dt = 0.1                # [sec]
 gamma = 0.5772          # Euler cste
 mu=1                    # Mu parameter for random acceleration Gumbel distribution
 beta=1                  # Beta parameter for random acceleration Gumbel distribution
+#       Define the state transition matrix
+A = np.eye(4)
+#       Define the ... matrix
+B = np.eye(4)
+#        Define the process noise covariance matrix
+Q = np.eye(4)
+#        Define the ... matrix
+P = np.eye(4)
+#        Define the ... matrix
+G = np.eye(4)
+#        Define the ... matrix
+C = np.eye(4)
+#        Define the ... matrix
+R = np.eye(4)
+
+# Useful onctions
+
+def kalman_gain(innovation_covariance,predicted_error_covariance):
+    return np.dot(np.dot(P, C.T), np.linalg.inv(np.dot(np.dot(C, P), C.T) + R))
+
+def predict_state_cov(s,u):
+    s[0] = np.dot(A,s[0]) - np.dot(B, u)
+    s[1] = np.dot(A,s[1]) - np.dot(B, u)
+    P = np.dot(np.dot(A, P), A.T) + Q
+    return s,P
+
+def update(K,s,y,P):
+    s[0] += np.dot(K,y-np.dot(C, s[0]))
+    s[1] += np.dot(K,y-np.dot(C, s[1]))
+    P -= np.dot(np.dot(K,C),P)
 
 # 1 : Initial conditions
 
@@ -25,11 +55,6 @@ P0 = np.diag([P0_pos,P0_pos,P0_vel,P0_vel])
 s_0_pos = np.random.normal(10,50,(n,n))
 s_0_vel = np.random.normal(10,10,(n,n))
 
-# Define the state transition matrix
-A = np.eye(4)
-# Define the process noise covariance matrix
-# Q = 
-
 # 2 : Run the filter
 #           --> X_pred_k = A*X_pred_k-1 + B*u_k + G (mu + beta*gamma, mu + beta*gamma)
 
@@ -37,4 +62,3 @@ for k in range(N):
     # Predict state and covariance matrix
     #Compute Kalman gain
     pass
-
